19ddb2aeb8494a871f550e5a4006f0854b710c90
kinyanjui-peter <01peterkinyanjui@gmail.com>
2022-12-09 09:26:58 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print alphabets in lower case
+ *
+ * Return : 0
+*/
+int main(void)
+{
+	int counter;
+
+for (counter = 'a'; counter <= 'z'; counter++)
+{
+putchar(counter);
+}
+printf("\n");
+return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints number 0 to 9
+ *
+ * Return  : 0
+ *
+*/
+int main(void)
+{
+	int counter;
+
+	for (counter = 0; counter < 10; counter++)
+	{
+		printf("%d", counter);
+	}
+	printf("\n");
+	return (0);
+}
+
