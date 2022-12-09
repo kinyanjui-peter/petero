1d4ef218970b49861185cf1a821482fcbd98735e
kinyanjui-peter <01peterkinyanjui@gmail.com>
2022-12-09 02:58:24 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -Print alphabetic in small and capital
+ *
+ * Return : 0
+ *
+*/
+int main(void)
+{
+	int counter;
+
+		for (counter = 'a'; counter <= 'z'; counter++)
+		{
+			putchar(counter);
+		}
+	for (counter = 'A'; counter <= 'Z'; counter++)
+	{
+		putchar(counter);
+	}
+	putchar('\n');
+	return (0);
+}
