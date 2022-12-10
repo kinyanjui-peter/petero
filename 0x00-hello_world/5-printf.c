cc3165d1f2808d5c5618ee440287199103e8ae22
kinyanjui-peter <01peterkinyanjui@gmail.com>
2022-12-10 12:08:38 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - printsa string of characters
+ *
+ * Return: o
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
