45432fdb68b691fc4cab9d4e251910c6fc99f01d
kinyanjui-peter <01peterkinyanjui@gmail.com>
2022-12-13 11:49:58 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- * main - prints the word _putchar
+ * main - prints _putcharfillowed by new line
  * written by : peter
  * Return : 0
  */
-
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
+	{
+_putchar('_');
+_putchar('p');
 _putchar('u')
-	_putchar('t');
+_putchar('t');
 _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
+{
 return (0);
 }
