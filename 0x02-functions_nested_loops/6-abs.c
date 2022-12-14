9b3cb411f5acee021ccfe5624a134067030b4031
kinyanjui-peter <01peterkinyanjui@gmail.com>
2022-12-13 16:43:56 -0800

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
- * _abs - prints absolute value
- *@n: the passed value
- * Return null
+ * _abs - Prints absolute value
+ * @n: the passed value
+ * Return: n
+ *
  */
+
 int _abs(int n)
 {
 	if (n > 0)
