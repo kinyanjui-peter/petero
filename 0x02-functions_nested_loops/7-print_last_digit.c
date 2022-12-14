5dc1baae48188c5e4a3ce8a9f46cf535f628bf80
kinyanjui-peter <01peterkinyanjui@gmail.com>
2022-12-13 16:11:23 -0800

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,21 +8,9 @@
 
 int print_last_digit(int x)
 {
-	int r = x % 10;
-if (x > 0)
-{
-	return (r);
-	{
-else if (x == 0)
-{
-	return (0);
-else
-{
-return (r);
-}
-}
-}
-_putchar('\n');
-return (0 + r);
-	}
+int y = x % 10;
 
+_putchar(y);
+_putchar('\n');
+return (0);
+}
