6c9c35a228ec934e9ea1aee481171f3bf7bb99ba
kinyanjui-peter <01peterkinyanjui@gmail.com>
2022-12-15 20:39:59 -0800

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Print diagonal
+ *@n: maximum number
+ * Return: 0
+ *
+ */
+void print_diagonal(int n)
+{
+	int x, y, z;
+
+	for (x = 1; x <= n; x++)
+	{
+		for (y = 1; y <= x; y++)
+		{
+			if (x == y)
+				_putchar('\\');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
+
---
+++ 0x04-more_functions_nested_loops/test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Print perten
+ *
+ * Return: void
+ *
+ */
+void main(void)
+{
+	int x, y, z;
+
+	printf("enter the number of multiplier");
+	scanf("%d", &x);
+
+	for (y == 0; y <= x; y++)
+	{
+		for (z = 0; z <= y; z++)
+		{
+			printf("%d", z);
+		}
+	printf("\n");
+	}
+}
