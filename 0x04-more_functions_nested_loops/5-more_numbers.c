9f34d6efc3a98bbbd6b146d61f8e1bda36b08cf4
kinyanjui-peter <01peterkinyanjui@gmail.com>
2022-12-15 19:16:13 -0800

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,10 +14,14 @@ void more_numbers(void)
 	for (x = 0; x <= 10; x++)
 	{
 		for (y = 0; y <= 14; x++)
-					{
-		_putchar(y);
+		{
+			if (y >= 10)
+			{
+				_putchar('1');
+			}
+		_putchar(y % 10 + '0');
+
 		}
 		_putchar('\n');
 	}
 }
-
