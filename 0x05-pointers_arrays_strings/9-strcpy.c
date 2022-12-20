4a7e9f11860d891845b023f26583062b3d1edb04
kinyanjui-peter <01peterkinyanjui@gmail.com>
2022-12-19 17:50:56 -0800

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies str pointed at;
+ * @dest: destination
+ * @src: pointer
+ * Return: return dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *s = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
+}
