f9a9e256b7e8d54615ab8492910c94fc5b3579a0
kinyanjui-peter <01peterkinyanjui@gmail.com>
2022-12-22 10:05:01 -0800

    task 5

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - prints inytergers in reverse
+ * @a: input
+ * @n: input
+ * Return: 0;
+ */
+
+void reverse_array(int *a, int n)
+{
+	int x, y;
+
+	for (x = 0; x < y; x++)
+	{
+	y = a[x];
+a[x] = a[n];
+a[n] = y;
+	}
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *string_toupper - lowercasto uppere letters
+ *@n: pointer
+ *
+ * Return: 0;
+ *
+ */
+
+char *string_toupper(char *n)
+{
+	int x;
+
+	x = 0;
+while (n[x] != '\0')
+	{
+	(n[x] >= 'a' && n[x] <= 'z')
+	n[x] = n[x] - 32;
+	x++;
+	}
+	return (n);
+}
