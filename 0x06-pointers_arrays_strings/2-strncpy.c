ea6db9f14872694c6dac272cbc833be8709de5f9
kinyanjui-peter <01peterkinyanjui@gmail.com>
2022-12-22 09:07:29 -0800

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ **_strncpy - copies two string
+ *@dest: destination
+ *@src: source
+ *@n: sze of dest
+ *Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	x = 0;
+	while (x < n && src[i] != '\0')
+	{
+		dest[x] = src[x];
+		x++;
+	}
+	while (x < n)
+	{
+	dest[x] = '\0';
+	x++;
+	}
+	return (dest);
+}
