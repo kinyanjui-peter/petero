d8fd81c8c4def9cbb0837cdd0a787297395a263c
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-01-04 13:49:10 -0800

    task 0

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -17,18 +17,18 @@
 void simple_print_buffer(char *buffer, unsigned int size)
 {
 	 unsigned int i;
-	 
-	i = 0;
-	while (i < size)
-	{
-		 if (i % 10)
-		 {
-			 printf(" ");
-		 }
-		 if (!(i % 10) && i)
-		 {
-			 printf("\n");
-		 }
+
+	 i = 0;
+while (i < size)
+{
+if (i % 10)
+{
+printf(" ");
+}
+if (!(i % 10) && i)
+{
+printf("\n");
+}
 		 printf("0x%02x", buffer[i]);
 		 i++;
 	}
