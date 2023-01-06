cb4a49e57f7b78eefa3904b78281aa189a73dabc
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-01-05 17:15:49 -0800

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print reverse
+ * length - Calculate the length
+ * @s: string
+ * Return: reversed string
+ */
+int length(char *s)
+{
+	if (*s == '\0')
+	return;
+	else
+	{
+		return ((1 + length(s + 1)));
+	}
+	return (0);
+}
+
+void _print_rev_recursion(char *s)
+{
+	int x;
+
+	x = s[length()];
+
+	if (x < 0)
+	return;
+	else
+	{
+		_putchar(*s);
+		_print_rev_recursion(s - 1);
+	}
+}
