413b77ca9f602a3ec40d84b46c1d6b3241e4cf95
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-01-07 02:05:22 -0800

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - gives the power of a number;
+ * @x: base number
+ *@y: poer number
+ *Return: power.
+ *
+ */
+int _pow_recursion(int x, int y)
+{
+	/*int  squared = 0;*/
+
+if (y < 1)
+	return (-1);
+else if (y == 1)
+	return (x);
+else if (y == 0)
+	return (1);
+else
+	return (x * _pow_recursion(x , y - 1));
+}
