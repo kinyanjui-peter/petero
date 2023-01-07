565f49f374dde6e42ba063101b3ac1ccb00dc9e2
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-01-07 03:30:29 -0800

    task 6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _prime_number - prints prime numbers
+ *@n: maximum number
+ *Return: 1 0r zero
+ *
+ */
+
+int is_prime_number(int n)
+{
+	int x;
+
+	if (n < 0)
+		return (-1);
+		if (n / 1 == n && n / (_prime_number(int n))  == n)
+		return (1);
+	else 
+		return (0);
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * is_prime_number - prints prime numbers
+ *@n: maximum number
+ *Return: 1 0r zero
+ *
+ */
+
+int is_prime_number(int n)
+{
+	{
+	if (n < 0)
+		return (-1);
+	if else (n / 1 == n && n / is_prime_number(int n) == n)
+		return (1);
+	else
+		return (0);
+	}
+	return (0);
+}
