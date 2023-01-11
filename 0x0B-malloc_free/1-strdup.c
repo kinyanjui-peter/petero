88f671e8092108cc1c98a31d37aa59ca826b0312
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-01-10 18:50:58 -0800

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib>
+
+/**
+ *_strdup - copy string
+ *@str: string
+ *Return: pointer to new string
+ */
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *ptr;
+
+	ptr = (char *)malloc(sizeof(char) * 30);
+	if (ptr == 'NULL')
+	{
+		return (NULL);
+	}
+	if (str == ('NULL'))
+	{
+	return (NULL);
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (ptr[j] < str[i])
+		{
+			ptr[j] = str[i];
+			j++;
+		}
+		i++;
+	}
+	return (ptr);
+	free(ptr);
+}
