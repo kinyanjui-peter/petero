4f4bfacb83ecb1777a39b51a66b6b85b28d73f51
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-01-10 17:41:04 -0800

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create and initialize an array of characters
+ * @size: size
+ * @c: character
+ * Return: pointer to return
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int x;
+	char *ptr;
+	
+		
+	ptr = (char *)malloc(sizeof(char) * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	for (x = 0; x < size; x++)
+	{
+	 c = _putchar(ptr[x]);
+	}
+	ptr[0] = 'P';
+	_putchar('\0');
+	return (ptr);
+	free(ptr);
+}
+
+
