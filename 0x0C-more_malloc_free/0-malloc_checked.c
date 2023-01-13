e338c699d7562d88508eb5ba87a85ae21dd1793a
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-01-13 14:02:37 -0800

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,18 +6,12 @@
  *Return: pointer to memory
  */
 void *malloc_checked(unsigned int b)
-{		 int a;
+{
 	unsigned int *p;
 
-		p = &b;
 		p = (unsigned int *)malloc(sizeof(int) * b);
 		if (p == NULL)
 		{
-			for (a = a - 1; a >= 0; a--)
-			{
-				free(p[a]);
-			}
-			free(p);
 			exit(98);
 		}
 		return (p);
