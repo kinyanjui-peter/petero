734f75b74b7e2b19eacbabee3bd18709c22e4251
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-01-12 17:47:31 -0800

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,13 +21,12 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	for (i = 0; *(s1 + i) != '\0'; i++)
+	for (i = 0; (s1[i]) != '\0'; i++)
 	{
-		*(p + i) = *(s1 + i);
-		for (j = 0; *(s2 + j) != '\0'; j++)
+		p[i] = s1[i];
+		for (j = 0; s2[j] != '\0'; j++)
 		{
-			*(p + i) = *(s2 + j);
-			i++;
+			p[i] = s2[j];
 		}
 		}
 	return (p);
