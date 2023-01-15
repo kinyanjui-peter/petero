4a5fd735acfadf168dc89786ae7422ac96b4f2d5
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-01-15 15:23:30 -0800

    task 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,15 +12,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned char *p;
 	unsigned int s1len, s2len;
 
-	s1len = 0;
-	while (s1 && s1[s1len])
-		s1len++;
-	s2len = 0;
-	while (s2 && s2[s2len])
-		s2len++;
-	if (n > s2len)
-	{
-		p = ((char *)malloc(sizeof(char)) * (s1len + s2len + 1));
+for (s1len; s1[s1len] '\0'; s1len++)
+for (s2len; s2[s2len] != '\0'; s2len++)
+if (n > s2len)
+{
+	p = ((char *)malloc(sizeof(char)) * (s1len + s2len + 1));
 		else
 			{
 			p = ((char *)malloc(sizeof(char)) * (s1len + n + 1));
@@ -30,15 +26,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 				return (NULL);
 			}
 		}
-		while (s1 < s1len)
+		for (x = 0; x < (s1len + s2len); x++)
 		{
+		if (x < s1len)
 		p[x] = s1[x];
-		{
-			while (s1 <= s2len)
-			{
-				p[x] = p[y == n];
-			}
+		else
+			p[x] = s2[x - s1len];
 		}
-		}
-		return (p);
+p[x] = '\0';
+return (p);
 }
