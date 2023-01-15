ec000b16d438076a08d43d4b65efb7e3e77ecb85
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-01-15 14:37:41 -0800

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,19 +8,20 @@
  *Return: pointer to s1
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
-{	unsigned int x;
+{	unsigned int x, y;
 	unsigned char *p;
-	unsigned int s1len = 0, s2len = 0;
+	unsigned int s1len, s2len;
 
+	s1len = 0;
 	while (s1 && s1[s1len])
 		s1len++;
+	s2len = 0;
 	while (s2 && s2[s2len])
 		s2len++;
 	if (n > s2len)
 	{
 		p = ((char *)malloc(sizeof(char)) * (s1len + s2len + 1));
-	}
-			else
+		else
 			{
 			p = ((char *)malloc(sizeof(char)) * (s1len + n + 1));
 			}
@@ -29,9 +30,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 				return (NULL);
 			}
 		}
-		for (x = 0; x < s1len; x++)
+		while (s1 < s1len)
 		{
 		p[x] = s1[x];
+		{
+			while (s1 <= s2len)
+			{
+				p[x] = p[y == n];
+			}
 		}
-		return (*p);
+		}
+		return (p);
 }
