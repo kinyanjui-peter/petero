de07ac19fffeb2b2330e2940d9217dbea6e04c49
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-01-18 12:41:48 -0800

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#ifndef FILE
+#define file __FILE__
+#endif
+/**
+ * main - print the name of the file
+ *Return: 0
+ *
+ */
+void main(void)
+{
+	printf("compiled by: %s\n", file);
+}
