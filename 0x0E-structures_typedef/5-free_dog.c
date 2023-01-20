9a059b7fad5b83018a3e7f35295edcf220bd6a7f
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-01-20 14:33:08 -0800

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free dog structure
+ * @d: first memeber;
+ * return: void
+ *
+ */
+void free_dog(dog_t *d)
+{
+	if (d == 0)
+	{
+		return;
+	}
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
