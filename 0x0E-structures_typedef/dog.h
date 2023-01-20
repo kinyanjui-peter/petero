81433d29526c47e75c2ec3c93ef3263a5f176f26
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-01-20 01:01:37 -0800

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,1 +1,22 @@
-#include "main.h"
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * define struct dog 
+ *
+ * it is a header file.
+ *
+ *contain prototype
+ *
+ */
+struct dog {
+	char * name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
