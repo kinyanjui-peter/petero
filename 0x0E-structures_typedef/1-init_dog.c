944e6d0a21ea66b91f009cb85a2f14a714932b32
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-01-20 03:44:05 -0800

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog -function name that return a void
+ * @d: pointer to the structure dog
+ * @name: pointer to owner.
+ * @age: pointer to age
+ * @owner: pointer to owner.
+ *Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+	d->name = Poppy;
+	d->age = 3.5;
+	d->owner Bob;
+	}
+}
