baf7c0a16d19c9f3426450bae369572a6883e049
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-01-20 06:41:10 -0800

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,10 +15,14 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * doggy - typedef name
+ */
+
 typedef struct dog doggy;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif
+#endif /*DOG*/
