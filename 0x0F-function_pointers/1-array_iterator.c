48d9cbce911a3c62270a3a702783453370ad9223
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-01-21 19:43:32 -0800

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,10 +11,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int x;
+	unsigned int x;
 
-if (array == NULL || action == NULL)
-	return;
+if (array && size > 0 && action)
+{
 for (x = 0; x < size; x++)
 	{
 	action(array[x]);
