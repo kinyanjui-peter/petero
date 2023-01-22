d7bdcf785c9f1b7ad32f84e4ebb396ae1ea2c005
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-01-21 18:46:52 -0800

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+#include  <stdio.h>
+
+/**
+ * array_iterator - printd array
+ *@array: array
+ *@size: array size
+ *@action: pointer to array
+ *Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+if (array == NULL ||  action == NULL)
+	return;
+for (x = 0; x < size; x++)
+	{
+	action(arry[x]);
+	}
+}
