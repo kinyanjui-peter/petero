47e5f7b36edd6125043d2a8c73312e3834972900
kinyanjui <01peterkinyanjui@gmail.com>
2023-01-25 22:18:38 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print base 10 numbers
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n);
+	return (0);
+}
+
