5d4f7aa7dc96ee5bd7086415127e7dbec73657a2
kinyanjui <01peterkinyanjui@gmail.com>
2023-01-27 00:50:27 -0800

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - prints tie table
+ *Return: 0;
+ */
+
+void times_table(void)
+{
+int r;
+int c;
+int mul = 0;
+int last;
+
+	for (r = 0; r < 10; r++)
+	{
+		mul = r;
+		for (c = 0; c < 10; c++)
+		{
+			mul = r * c;
+			if (mul < 10)
+			{
+			_putchar(mul + '0');
+			_putchar(',');
+			_putchar(' ');
+			}
+			else
+			{
+			_putchar(mul/ 10 + '0'); 
+			last = (mul % 10);
+			_putchar(last + '0');
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+		_putchar('$');
+		_putchar('\n');
+	}
+}
