1b2be4a78551216f65adeef284a00a30eec6675b
kinyanjui <01peterkinyanjui@gmail.com>
2023-01-30 17:11:20 -0800

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -19,7 +19,7 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\nt ");
+			printf("[0] (nil)\n");
 		else
 		printf("[%d] %s", h->len, h->str);
 		h = h->next;
