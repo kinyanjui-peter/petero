99fb9d703c5015795430d2e2c146546704677783
kinyanjui <01peterkinyanjui@gmail.com>
2023-02-02 08:21:41 -0800

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - fre list
+ *@head: head
+ *
+ *Return: 0;
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *temp, *nextone;
+
+temp = *head;
+while (temp != NULL)
+{
+	nextone = current;
+	nextone = nextone->next;
+	free(current;
+}
+}
