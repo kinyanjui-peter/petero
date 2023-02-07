b75a88e133703945c13f71501cb49de93670b334
kinyanjui <01peterkinyanjui@gmail.com>
2023-02-06 18:56:20 -0800

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,14 +7,14 @@
 void print_binary(unsigned long int n)
 {
 	int i, c = 0;
-	unsigned int cur;
+	unsigned long int cur;
 
 	for (i = 63; i >= 0; i--)
 	{
 		cur = n >> 1;
 		if (cur & 1)
 		{
-			_putchar(1);
+			_putchar('1');
 			c++;
 		}
 		else if (c)
