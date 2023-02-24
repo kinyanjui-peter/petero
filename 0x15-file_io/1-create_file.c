c7a889665218f66d4f0986db46ac71eafa27c1bd
kinyanjui <01peterkinyanjui@gmail.com>
2023-02-23 18:03:12 -0800

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,12 +13,13 @@ int create_file(const char *filename, char *text_content)
 		return(-1);
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len])
+		for (len = 0; text_content[len];)
 				len++;
 	}
-	o = open(filename, O_CREATE | O_RDWR | O_TRUNC, NULL);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, NULL);
+	w = write(o, text_content, len);
 	if (o == -1 || w == -1)
 		return(-1);
-	fclose(o);
-	return (0);
+	close(o);
+	return (1);
 }
