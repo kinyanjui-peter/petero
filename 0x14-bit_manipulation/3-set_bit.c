34f73da2624e6ac36572fe5e3b3c5a17c37a032b
kinyanjui <01peterkinyanjui@gmail.com>
2023-02-26 13:22:14 -0800

    task3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,10 +7,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index < 63)
+if (index >= sizeof(unsigned long int) * 8)	
 	{
 		return (-1);
 	}
-	(*n = (1uL << index) * n);
+	(*n |= (1ul << index));
 	return (1);
 }
