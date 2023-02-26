3ec13445d2f40bc7872372a11f3221c5ffa100ed
kinyanjui <01peterkinyanjui@gmail.com>
2023-02-26 13:08:52 -0800

    task2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,11 +1,16 @@
 #include "main.h"
 /**
- * get_bit - gets the index of  BIT
+ * get_bit - gets the index bit of  
  *@n: the number
  *@index: index
  *Return: index
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	return (n & (1 << index)) != 0;
+	int x = 0;
+
+	if (index > 63)
+		return (-1);
+	x = (n >> index) & 1;
+	return (x);
 }
