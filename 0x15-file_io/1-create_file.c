0e98bc8361ddfe1d3002ca88687b21fc857b2abb
kinyanjui <01peterkinyanjui@gmail.com>
2023-02-27 12:00:09 -0800

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,18 +3,20 @@
  * create_file - function that create a file
  *@filename: name of file
  *@text_content: text to be copied to filename
- *Return:-1
+ *Return: 1
  */
+
 int create_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
-		       len++;
+			len++;
 	}
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
