1d1f5e294b473eca946411a0ecd2d7862b68e178
kinyanjui <01peterkinyanjui@gmail.com>
2023-02-27 22:39:43 -0800

    task102

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,67 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _ra - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list to append
+ * @size: size of the new list (always one more than the old list)
+ * @new: new node to add to the list
+ *
+ * Return: pointer to the new list
+ */
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+{
+	listint_t **nxtlist;
+	size_t i;
+
+	nxtlist = malloc(size * sizeof(listint_t *));
+	if (nxtlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		nxtlist[i] = list[i];
+	nxtlist[i] = new;
+	free(list);
+	return (nxtlist);
+}
+
+/**
+ * free_listint_safe - frees a listint_t linked list.
+ * @head: double pointer to the start of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t free_listint_safe(listint_t **head)
+{
+	size_t i, y = 0;
+	listint_t **list = NULL;
+	listint_t *next;
+
+	if (head == NULL || *head == NULL)
+		return (y);
+	while (*head != NULL)
+	{
+		for (i = 0; i < y; i++)
+		{
+			if (*head == list[i])
+			{
+				*head = NULL;
+				free(list);
+				return (y);
+			}
+		}
+		y++;
+		list = _ra(list, y, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+	free(list);
+	return (y);
+}
+Footer
+© 2023 GitHub, Inc.
