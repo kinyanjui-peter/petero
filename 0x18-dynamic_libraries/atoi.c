06101d1755540153780feb62ef6c7ad8b97ed7d0
kinyanjui <01peterkinyanjui@gmail.com>
2023-03-21 11:04:35 -0700

    task0

---
+++ 0x18-dynamic_libraries/atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _atoi - String to interger
+ * @s: Pointer
+ *
+ * Return: void
+ */
+
+int _atoi(char *s)
+{
+        int x;
+        unsigned int n;
+        char *t;
+
+        x = 1;
+        n = 0;
+        t = s;
+
+        while (*t != '\0' && (*t < '0' || *t > '9'))
+        {
+                if (*t == '-')
+                        x *= -1;
+                t++;
+        }
+        if (*t != '\0')
+        {
+                do {
+                        n = n * 10 + (*t - '0');
+                        t++;
+                } while (*t >= '0' && *t <= '9');
+        }
+        return (0);
+}
