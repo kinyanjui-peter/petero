5a3f5a36aede86917b70fd00686cd1812dcbe33f
kinyanjui <01peterkinyanjui@gmail.com>
2023-03-24 12:49:01 -0700

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -20,10 +20,10 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-		printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		count++;
 		h = h->next;
 	}
 	return (count);
-}I
+}
