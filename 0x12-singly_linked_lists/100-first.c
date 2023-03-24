aa4efa69b07aaec4bb287d2c23d83095806d09df
kinyanjui <01peterkinyanjui@gmail.com>
2023-03-24 15:27:11 -0700

    task100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,7 @@ void print1(void)__attribute__((constructor));
 
 void print1(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-    return;
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+	return;
 }
