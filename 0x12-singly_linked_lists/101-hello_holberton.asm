fa0e6eb3826dba539221b51486a42a0e74156160
kinyanjui <01peterkinyanjui@gmail.com>
2023-03-24 15:43:25 -0700

    task101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,15 @@
 section .data
-    msg db "Hello, Holberton!", 0Ah, 0
-
+	msg db "Hello, Holberton!", 0Ah, 0
 section .text
-    global main
-
-    extern printf
-
+	global main
+	extern printf
 main:
-    push rbp
-    mov rbp, rsp
-
-    lea rdi, [msg]
-    mov eax, 0
-    call printf
-
-    mov rsp, rbp
-    pop rbp
-    xor eax, eax
-    ret
+	push rbp
+	mov rbp, rsp
+	lea rdi, [msg]
+	mov eax, 0
+	call printf
+	mov rsp, rbp
+	pop rbp
+	xor eax, eax
+	ret
