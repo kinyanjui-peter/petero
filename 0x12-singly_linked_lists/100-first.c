0f7652abe2c84b0ac1e0478cec3fa1d82af94a57
kinyanjui <01peterkinyanjui@gmail.com>
2023-03-24 15:23:13 -0700

    task100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * print1 - prints before main.
+ * Rteurn: 0;
+ */
+void print1(void)__attribute__((constructor));
+
+void print1(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+    return;
+}
