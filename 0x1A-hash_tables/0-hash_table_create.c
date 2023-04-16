ca93320b232f4b0917fa3a0bf123113793a52cc6
kinyanjui <01peterkinyanjui@gmail.com>
2023-04-16 01:05:13 -0700

    task0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,6 +7,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
+	unsigned long int x;
 
 	hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
@@ -19,9 +20,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(hash_table);
 		return (NULL);
 	}
-	for (x = 0; x <= size; x++)
+	for (x = 0; x<=size; x++)
 	{
-		hash_table->array[i] = NULL;
+		hash_table->array[x] = NULL;
 	}
 	hash_table->size = size;
 	return (hash_table);
