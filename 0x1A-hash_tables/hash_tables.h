e882bc75f4467476e5118e6d36892c639c312220
kinyanjui <01peterkinyanjui@gmail.com>
2023-04-16 01:11:55 -0700

    task0

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -15,10 +15,10 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
-}hash_node_t;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
@@ -30,9 +30,9 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
-}hash_table_t;
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
